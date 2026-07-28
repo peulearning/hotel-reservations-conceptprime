1d8fa6f5f2f21561588556b9fddb3c62040c7905
Pedro <peuhenry2018@gmail.com>
2026-07-28 00:21:52 -0300

    feat: implents scheduling in main file / application.properties ✨

--- src/main/java/com/pedro/hotel_reservation_integration/HotelReservationIntegrationApplication.java
+++ src/main/java/com/pedro/hotel_reservation_integration/HotelReservationIntegrationApplication.java
@@ -2,8 +2,10 @@ package com.pedro.hotel_reservation_integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HotelReservationIntegrationApplication {
 
 	public static void main(String[] args) {

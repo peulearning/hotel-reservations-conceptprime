dc70ace57124f3eff5b932f8c262ec0aa2dfbbb2
Pedro <peuhenry2018@gmail.com>
2026-07-27 23:47:43 -0300

    Initial Commits ✅

---
+++ src/test/java/com/pedro/hotel_reservation_integration/HotelReservationIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.hotel_reservation_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
